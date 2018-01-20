e44b5d839cd04c9eb534f1bdcf483aeef7c60c4c
akshay0297 <akshay0297@gmail.com>
2018-01-20 09:12:53 +0530

    Main Commit #1

---
+++ telnet_client/telnet_client/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace telnet_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startConnection = new System.Windows.Forms.Button();
+            this.clientConnect = new System.Windows.Forms.Button();
+            this.toSender = new System.Windows.Forms.TextBox();
+            this.fromClient = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.ips = new System.Windows.Forms.Label();
+            this.ports = new System.Windows.Forms.Label();
+            this.ipc = new System.Windows.Forms.Label();
+            this.portc = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bw1 = new System.ComponentModel.BackgroundWorker();
+            this.bw2 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // startConnection
+            // 
+            this.startConnection.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.startConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startConnection.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startConnection.Location = new System.Drawing.Point(590, 45);
+            this.startConnection.Name = "startConnection";
+            this.startConnection.Size = new System.Drawing.Size(199, 36);
+            this.startConnection.TabIndex = 0;
+            this.startConnection.Text = "Start Server";
+            this.startConnection.UseVisualStyleBackColor = false;
+            this.startConnection.Click += new System.EventHandler(this.startConnection_Click);
+            // 
+            // clientConnect
+            // 
+            this.clientConnect.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clientConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientConnect.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.clientConnect.Location = new System.Drawing.Point(590, 140);
+            this.clientConnect.Name = "clientConnect";
+            this.clientConnect.Size = new System.Drawing.Size(199, 34);
+            this.clientConnect.TabIndex = 1;
+            this.clientConnect.Text = "Connect To Server";
+            this.clientConnect.UseVisualStyleBackColor = false;
+            this.clientConnect.Click += new System.EventHandler(this.clientConnect_Click);
+            // 
+            // toSender
+            // 
+            this.toSender.Location = new System.Drawing.Point(40, 226);
+            this.toSender.Multiline = true;
+            this.toSender.Name = "toSender";
+            this.toSender.Size = new System.Drawing.Size(766, 147);
+            this.toSender.TabIndex = 2;
+            // 
+            // fromClient
+            // 
+            this.fromClient.Location = new System.Drawing.Point(40, 413);
+            this.fromClient.Multiline = true;
+            this.fromClient.Name = "fromClient";
+            this.fromClient.Size = new System.Drawing.Size(637, 40);
+            this.fromClient.TabIndex = 4;
+            // 
+            // send
+            // 
+            this.send.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.send.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.send.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.send.Location = new System.Drawing.Point(709, 413);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(124, 40);
+            this.send.TabIndex = 5;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = false;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(116, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(405, 140);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // ips
+            // 
+            this.ips.AutoSize = true;
+            this.ips.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ips.Location = new System.Drawing.Point(65, 59);
+            this.ips.Name = "ips";
+            this.ips.Size = new System.Drawing.Size(22, 16);
+            this.ips.TabIndex = 10;
+            this.ips.Text = "IP";
+            // 
+            // ports
+            // 
+            this.ports.AutoSize = true;
+            this.ports.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ports.Location = new System.Drawing.Point(336, 55);
+            this.ports.Name = "ports";
+            this.ports.Size = new System.Drawing.Size(36, 16);
+            this.ports.TabIndex = 11;
+            this.ports.Text = "Port";
+            // 
+            // ipc
+            // 
+            this.ipc.AutoSize = true;
+            this.ipc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipc.Location = new System.Drawing.Point(65, 144);
+            this.ipc.Name = "ipc";
+            this.ipc.Size = new System.Drawing.Size(22, 16);
+            this.ipc.TabIndex = 12;
+            this.ipc.Text = "IP";
+            // 
+            // portc
+            // 
+            this.portc.AutoSize = true;
+            this.portc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.portc.Location = new System.Drawing.Point(336, 141);
+            this.portc.Name = "portc";
+            this.portc.Size = new System.Drawing.Size(36, 16);
+            this.portc.TabIndex = 13;
+            this.portc.Text = "Port";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(344, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Server";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(344, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Client";
+            // 
+            // bw1
+            // 
+            this.bw1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw1_DoWork);
+            // 
+            // bw2
+            // 
+            this.bw2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bw2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 485);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.portc);
+            this.Controls.Add(this.ipc);
+            this.Controls.Add(this.ports);
+            this.Controls.Add(this.ips);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.fromClient);
+            this.Controls.Add(this.toSender);
+            this.Controls.Add(this.clientConnect);
+            this.Controls.Add(this.startConnection);
+            this.Name = "Form1";
+            this.Text = "Telnet_Demo_Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startConnection;
+        private System.Windows.Forms.Button clientConnect;
+        private System.Windows.Forms.TextBox toSender;
+        private System.Windows.Forms.TextBox fromClient;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label ips;
+        private System.Windows.Forms.Label ports;
+        private System.Windows.Forms.Label ipc;
+        private System.Windows.Forms.Label portc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.ComponentModel.BackgroundWorker bw1;
+        private System.ComponentModel.BackgroundWorker bw2;
+    }
+}
+
---
+++ telnet_client/telnet_client/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using SimpleTCP;
+using System.Threading;
+
+
+
+namespace telnet_client
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader sr , sr1;
+        public StreamWriter sw , sw1;
+        public string recv;
+        public string tosend;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            IPAddress[] localIp = Dns.GetHostAddresses(Dns.GetHostName());
+
+            /* Stores IP Addresses returned by 
+           GetHostAddresses using HostName in Byte format in localIp array
+           */
+            foreach (IPAddress addr in localIp)
+            {
+                if (addr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    continue;
+                }
+
+            }
+
+        }
+        
+        private void startConnection_Click(object sender, EventArgs e) //Start Server Button
+        {
+            TcpListener list = null;
+            try
+            {
+                list = new TcpListener(IPAddress.Any, int.Parse(textBox2.Text));
+                    /*
+                     * Creates Instance of the TcpListener class that listens for incoming 
+                     * connection attempts on the 
+                     * specified local IP address and port number
+                     */
+                    list.Start();
+                    /* starts listening for incoming connection requests */
+                    MessageBox.Show("Server Started on IP : " + textBox1.Text + " Port Number : " + textBox2.Text + "  !! Listening For Clients !!");
+                    toSender.AppendText("Server Started !! \n");
+                    this.fromClient.ReadOnly = true;
+
+                
+                
+                    client = list.AcceptTcpClient();
+
+                
+                    /*
+                    * TcpListener instance accepts the pending connection 
+                    * and assign it as client
+                    */
+
+
+                    sr = new StreamReader(client.GetStream());
+                    sr1 = new StreamReader(client.GetStream());
+                    /*
+                     * sr takes the type of stream to receive data that 
+                     * is used by client to send data 
+                     * GetStream() return the NetworkStream used by client to send or receive data
+                     */
+
+                    sw = new StreamWriter(client.GetStream());
+                    sw1 = new StreamWriter(client.GetStream());
+                    /*
+                     * sw takes the type of stream to send data that
+                     * is used by client to receive data 
+                     */
+                    sw.AutoFlush = true;
+                    sw1.AutoFlush = true;
+                    /*
+                     * To clear the stream writer buffer                 
+                     */
+                    bw2.RunWorkerAsync();
+                    /*
+                     * Allows Multiple Application to work in background 
+                     */
+                    bw1.WorkerSupportsCancellation = true;
+                    /*
+                     * Allows backgroundWorker to cancel asynchronously 
+                     */
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Server Starting Error with exception : " + ex.ToString());
+            }
+
+        }
+
+        private void clientConnect_Click(object sender, EventArgs e) // connect to server button 
+        {
+            client = new TcpClient();
+            /* 
+             * Creating new TcpClient Instance 
+             */
+            IPEndPoint ipend = new IPEndPoint(IPAddress.Parse(textBox3.Text), int.Parse(textBox4.Text));
+            /*
+             * Creates an instance of IpEndPoint which holds the IP address and 
+             * port number needed by application to connect to the server 
+             */
+            try
+            {
+                client.Connect(ipend);
+                /*
+                 * Connecting to server using the above port number and ip address 
+                 */
+                if (client.Connected)
+                {
+                    toSender.AppendText("Connected To Server " + "\n");
+                    sr = new StreamReader(client.GetStream());
+                    sw = new StreamWriter(client.GetStream());
+                    sr1 = new StreamReader(client.GetStream());
+                    sw1 = new StreamWriter(client.GetStream());
+                    sw.AutoFlush = true;
+                    sw1.AutoFlush = true;
+
+                    bw2.RunWorkerAsync();
+                    bw1.WorkerSupportsCancellation = true;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection Failed with exception : " + ex.ToString());
+            }
+
+        }
+
+        private void send_Click(object sender, EventArgs e) // Send Button 
+        {
+            try
+            {
+                if (fromClient.Text != "")
+                {
+                    tosend = fromClient.Text;
+                    bw1.RunWorkerAsync();
+                }
+                fromClient.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Couldn't  Send The Data ");
+            }
+        }
+        private void bw1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                sw.WriteLine(tosend);
+                this.fromClient.Invoke(new MethodInvoker(delegate ()
+                { toSender.AppendText("Client Sending : " + tosend + "\n"); }));
+                string r = tosend;
+                r.ToCharArray();
+                int i;
+                for (i = 0; i < r.Length; i++)
+                {
+                    this.fromClient.Invoke(new MethodInvoker(delegate ()
+                    { toSender.AppendText("Server Sent : " + r[i] + "\n"); }));
+                    
+                }
+                sw.Flush();
+            }
+            else
+            {
+                MessageBox.Show("Sending Failed !!! ");
+            }
+           
+            bw1.CancelAsync();
+
+        }
+
+    private void bw2_DoWork(object sender, DoWorkEventArgs e)
+    {
+        while (client.Connected)
+        {
+            try
+            {
+                recv = sr.ReadLine();
+                this.toSender.Invoke(new MethodInvoker(delegate ()
+                { toSender.AppendText("Client Sent : " + recv + "\n"); }));
+                /*
+                 * To Receive the data sent by client in background
+                 */
+                recv.ToCharArray();
+               
+                    int i = 0;
+
+                for (i = 0; i < recv.Length; i++)
+                {
+                    this.toSender.Invoke(new MethodInvoker(delegate ()
+                    { toSender.AppendText("Server Sending : " + recv[i] + "\n"); }));
+                        /*
+                         * Invokes bacgkroundWorker2 textbox to write data into it 
+                         */
+        
+                 }
+                recv = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Server Sending Fail with exception : " + ex.ToString());
+            }
+
+            /*
+             * Requests cancellation of pending Operations
+             */
+        }
+    }
+    }
+}
